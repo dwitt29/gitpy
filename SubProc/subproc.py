affc867090f9c36328069a0eac8343859c48f999
dave <dwitt29@verizon.net>
2017-11-13 21:25:59 -0500

    cleaned up subproc.py

--- SubProc/subproc.py
+++ SubProc/subproc.py
@@ -1,17 +1,3 @@
-print("Hello, world!")
-
-r = open('/projects/r.txt', "r")
-w = open('/projects/w.txt', "w")
-list=[]
-for i in r:
-    print(i.strip())
-    list.append(i.strip())    
-    w.write(i.strip().upper()+"\n")
- 
-f='be' 
-for i in list:
-    if f in i: print("found %s in %s" % (f,i))
-    
 import sys
 #sys.exit(0)
 import subprocess
