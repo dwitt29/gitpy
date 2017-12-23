20be4e3325d89c5bbd1c0fad39028c40b780a1e0
dwitt29 <dwitt29@verizon.net>
2017-12-23 01:31:10 -0500

    functional rpm_collector.py

--- junk/rpm_collector.py
+++ junk/rpm_collector.py
@@ -2,16 +2,33 @@
 
 import os
 import re
-
-rpms=os.popen(('rpm -qa'))
-pattern=re.compile(r"^Name\s+|^Install Date")
+from datetime import datetime
+import collections
 
 def main():
 
-    for rpm in rpms:
-        info=os.popen(('rpm -qi {}'.format(rpm))).read().splitlines()
-        for i in [ i for i in (i.split(':',1) for i in info if re.search(pattern,i)) ]:
-            try: print dict([[a.strip() for a in i]]) 
-            except: pass
+    nameregex=re.compile("^Name\s+:\s+(.+)")
+    versionregex=re.compile("^Version\s+:\s+(.+)")
+    installregex=re.compile("^Install Date:\s+(.+)")
+    info=os.popen(('rpm -qai {}')).read().splitlines()
+    rpmname=''; rpminstall=''; rpmversion=''
+    rpms={}
+    for i in info:
+        try: rpmname=re.match(nameregex,i).group(1)
+        except: pass
 
-main()
+        try: rpmversion=re.match(versionregex,i).group(1)
+        except: pass
+
+        try: 
+            rpminstall=re.match(installregex,i).group(1)
+            # eg. Sat 19 Nov 2016 09:44:13 PM EST
+            dt=datetime.strptime(rpminstall,'%a %d %b %Y %I:%M:%S %p %Z' )
+            rpms.update({rpmname:{'InstallDate':dt,'Version':rpmversion}})
+            rpmname=''; rpminstall=''; rpmversion=''
+        except: pass
+    ordered=collections.OrderedDict(sorted(rpms.items(), key=lambda t: t[1]['InstallDate']))
+    for k,v in ordered.iteritems(): print k,v
+        
+if __name__ == '__main__':
+    main()
