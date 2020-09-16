7a5abc82fccc644723bfae194ee98dce2cb849c6
Dave <dwitt29@verizon.net>
2020-09-15 20:38:08 -0400

    add threetimes.py

---
+++ DailyCodingProblem/threetimes.py
@@ -0,0 +1,22 @@
+
+# Daily Coding Problem: Problem #549 [Hard] Sept 14, 2020
+
+a = [ 13,19,13,13 ]
+onlyone=list()
+hash=dict()
+
+for i in a:
+    print(i)
+    if not i in hash:
+        hash[i]=1
+        onlyone.append(i)
+        print('adding {} to onlyone[]={}'.format(i, onlyone))
+    else:
+        try:
+            onlyone.remove(i)
+            print('removing {} to onlyone[]={}'.format(i, onlyone))
+        except:
+            pass
+
+print(onlyone)
+
